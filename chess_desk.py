1ba106b8b146f8f2e01bd218001c2a66c80c09b0
NoPiece <neggrr@gmail.com>
2018-03-14 22:53:39 +0200

    chess_desk and converts

---
+++ chess_desk.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+#Filename: chess_desk.py
+''' Programm for printing chess field by height and width which controlled by user.
+    Used python 3.5'''
+
+def main():
+  mistake = True
+  while mistake == True:
+    user_height = input('Please input height value ')
+    user_width = input('Please input width value ')
+
+    if user_height == 'q' or user_width == 'q':
+      sys.exit(1)
+
+    try:
+      height = int(user_height)
+      width = int(user_width)
+      mistake = False
+    except:
+      print('Use integers for width and height or "q" to leave')
+      
+    field = FieldGenerator(height, width)
+    print(field.string_concat())   
+    
+class FieldGenerator:
+  ''' Class for creation chess field '''
+  def __init__(self, height, width):
+    self.height = height
+    self.width = width
+
+  def string_concat(self):
+    result = ''
+    first_string = '* ' * self.width + '\n'
+    second_string = ' *' * self.width + '\n'
+    for i in range(self.height):
+      if i % 2 ==0:
+        result += first_string
+      else:
+        result += second_string  
+    return result    
+
+if __name__ == "__main__":
+  main()
+  
---
+++ converts.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#Filename: converts.py
+
+def main():
+  play = True
+  while play:
+    try:
+      f_height = float(input('First convert height: '))
+      f_width = float(input('First convert width: '))
+      s_height = float(input('Second convert height: '))
+      s_width = float(input('Second convert width: '))
+    except:
+      print('Use integets or floating point numbers')
+      
+    computer(f_height, f_width, s_height, s_width)
+    answer = input('Do you want play again? ').lower()
+    if answer != 'y' and answer != 'yes':
+      print('Thank you!')
+      play = False
+      
+
+def computer(f_height, f_width, s_height, s_width):
+  if f_height < s_height and f_width < s_width:
+    print('True')
+  elif f_height > s_height and f_width > s_width:
+    print('True')
+  else: 
+    print('False')
+
+if __name__ == "__main__":
+  main()
+    
