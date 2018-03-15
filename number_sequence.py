570bd9f7cc635b55152d93f55c8d380171c2484a
NoPiece <neggrr@gmail.com>
2018-03-15 15:41:15 +0200

    add number_sequence.py

---
+++ number_sequence.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#Filename: number_sequence.py
+
+import sys
+
+def main():
+  try:
+    n = int(input('Please input top border for sequence:\n'))
+  except:
+    print('Use integers, please.')
+    sys.exit(1)
+
+  obj = SequenceGenerator(n)
+  print(obj.sequence())
+    
+    
+class SequenceGenerator():
+  ''' Class for generation sequence of natural numbers '''
+  def __init__(self, n):
+    self.n = n
+    
+  def sequence(self):
+    i = 0
+    result = []
+    while self.n > pow(i, 2):
+      result.append(i)
+      i += 1
+    return str(result)[1:-1]
+  
+    
+if __name__ == '__main__':
+  main()
+  
